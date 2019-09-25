0bce34f650bd7c9105ffc4e39eac9a11a7b88429
Zaburik <48108195+Zaburik@users.noreply.github.com>
2019-09-25 16:03:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -32,6 +32,10 @@ function getQuantityElements(heightElement) {
 function startGame(){
     gameArea.classList.remove(`hide`);
     start.classList.add(`hide`);
+    gameArea.innerHTML = ``;
+    car.style.left = `125px`;
+    car.style.top = `auto`;
+    car.style.bottom = `10px`;
     for(i = 0; i < 20; i++){
         const line = document.createElement(`div`);
         line.classList.add(`line`);
@@ -50,6 +54,7 @@ function startGame(){
         gameArea.appendChild(enemy);
 
     }
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
     setting.x = car.offsetLeft;
@@ -59,6 +64,8 @@ function startGame(){
 function playGame(){
     
     if(setting.start){
+        setting.score += setting.speed;
+        score.innerHTML = `Score<br>` + setting.score;
         moveRoad();
         moveEnemy();
         if(keys.ArrowLeft && setting.x > 0){
@@ -96,7 +103,7 @@ function moveRoad(){
       line.y += setting.speed;
       line.style.top = line.y + `px`; 
 
-      if(line.y > document.documentElement.clientHeight){
+      if(line.y >= document.documentElement.clientHeight){
           line.y = -100;
       }
     });
@@ -105,12 +112,25 @@ function moveRoad(){
 function moveEnemy(){
     let enemy = document.querySelectorAll(`.enemy`);
     enemy.forEach(function(item){
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+    if(carRect.top <= enemyRect.bottom &&
+        carRect.right >= enemyRect.left &&
+        carRect.left <= enemyRect.right &&
+        carRect.bottom >= enemyRect.top){
+          setting.start = false;
+          console.warn(`ДТП`);
+          start.classList.remove(`hide`);
+          start.style.top = score.offsetHeight;
+    }
+
        item.y += setting.speed / 2;
        item.style.top = item.y + `px`;
 
-       if(item.y > document.documentElement.clientHeight){
+       if(item.y >= document.documentElement.clientHeight){
            item.y = -100 * setting.traffic;
            item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + `px`;
        }
     });
 }
+
